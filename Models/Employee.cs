b70caf7788b8de98ea35ac31739a48f91a3ae425
Emmanuel Sarpong <83601745+emmanuelsarpong@users.noreply.github.com>
2021-08-19 07:28:52 -0400

    Update Employee.cs

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -8,12 +8,12 @@ using System.Web;
 
 public class EmployeesItem
 {
-[Key]
+    [Key]
     public long id { get; set; }
-    public string last_name { get; set; }
-    public string first_name { get; set; }
-    public string title { get; set; }
+    // public string last_name { get; set; }
+    // public string first_name { get; set; }
+    // public string title { get; set; }
     public string email { get; set; }
-
-    public string encrypted_password { get; set;}
+    // public string encrypted_password { get; set;}
 }
+
